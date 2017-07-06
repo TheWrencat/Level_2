2738c43b894961927117df65b44a9530afef87af
TheWrencat <league@WTS-iMac-11.local>
2017-07-05 19:58:47 -0700

    Jump game progress- on to ref pt.6
    bugs with moving controls

--- src/JumpObjects.java
+++ src/JumpObjects.java
@@ -1,4 +1,41 @@
+import java.awt.Graphics;
 
 public class JumpObjects {
+	// Member variables
+	JumpPanel panel;
+	int x;
+	int y;
+	int width;
+	int height;
+
+	boolean up;
+	boolean down;
+	boolean left;
+	boolean right;
+
+	JumpObjects() {
+		panel = new JumpPanel();
+	}
+
+	void update() {
+		// collisionBox.setBounds(x, y, width, height);
+		if (up == true) {
+			y -= 5;
+		}
+		if (down == true) {
+			y += 5;
+		}
+		if (right == true) {
+			x += 5;
+		}
+		if (left == true) {
+			x -= 5;
+		}
+
+	}
+
+	void draw(Graphics g) {
+		g.fillRect(x, y, 100, 100);
+	}
 
 }
--- src/JumpPanel.java
+++ src/JumpPanel.java
@@ -1,5 +1,4 @@
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,94 +9,92 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class JumpPanel extends JPanel implements ActionListener, KeyListener {
-	//Member Variables
-	//menu states
+	// Member Variables
+	JumpObjects objects;
+	// menu states
 	final int MENU_STATE = 0;
 	final int GAME_STATE = 1;
 	final int END_STATE = 2;
 	int currentState = MENU_STATE;
-	//timer
+	// timer
 	Timer timer;
-	
-	//Constructor
-	JumpPanel(){
-		timer = new Timer(1000/60, this);
-		
-	}
-	
-	void startGame(){
-		
-		timer.start();
-		
+
+	// Constructor
+	JumpPanel() {
+		objects = new JumpObjects();
+		timer = new Timer(1000 / 60, this);
+
 	}
 
-	//Draw States
-	void drawMenuState(Graphics g){
+	void startGame() {
+
+		timer.start();
+
+	}
+
+	// Draw States
+	void drawMenuState(Graphics g) {
 		g.setColor(Color.BLUE);
 		g.drawRect(0, 0, JumpRunner.FRAME_WIDTH, JumpRunner.FRAME_HEIGHT);
-		g.setColor(Color.WHITE);
-		g.drawString("Jump", 70, 200);
-		g.drawString("START GAME", 85, 250);
+		// g.setColor(Color.WHITE);
+		// g.drawString("Jump", 70, 200);
+		// g.drawString("START GAME", 85, 250);
 	}
-	void drawGameState(Graphics g){
-		g.setColor(Color.WHITE);
-		g.drawRect(0, 0, JumpRunner.FRAME_WIDTH, JumpRunner.FRAME_HEIGHT);
-		
+
+	void drawGameState(Graphics g) {
+		// g.setColor(Color.WHITE);
+		// g.drawRect(0, 0, JumpRunner.FRAME_WIDTH, JumpRunner.FRAME_HEIGHT);
+
 	}
-	void drawEndState(Graphics g){
-		g.setColor(Color.RED);
-		g.drawRect(0, 0, JumpRunner.FRAME_WIDTH, JumpRunner.FRAME_HEIGHT);
-		g.setColor(Color.WHITE);
-		g.drawString("GAME OVER", 70, 200);
-		g.drawString("RESTART", 85, 250);
+
+	void drawEndState(Graphics g) {
+		// g.setColor(Color.RED);
+		// g.drawRect(0, 0, JumpRunner.FRAME_WIDTH, JumpRunner.FRAME_HEIGHT);
+		// g.setColor(Color.WHITE);
+		// g.drawString("GAME OVER", 70, 200);
+		// g.drawString("RESTART", 85, 250);
 	}
-	
-	//Update States
-	void updateMenuState(){
-		
+
+	// Update States
+	void updateMenuState() {
+
 	}
-	
-	void updateGameState(){
-	
+
+	void updateGameState() {
+
 	}
-	
-	void updateEndState(){
-		
+
+	void updateEndState() {
+
 	}
-	
-	//Paint
-	public void paintComponent(Graphics g){
-		g.fillRect(10, 10, 100, 100);
-		
-//		if(currentState == MENU_STATE){
-//			drawMenuState(g);
-//			
-//			
-//		}else if (currentState == GAME_STATE){
-//			drawGameState(g);
-//		}else if(currentState == END_STATE){
-//			drawEndState(g);
-//		}
+
+	// Paint
+	public void paintComponent(Graphics g) {
+		objects.draw(g);
+		if (currentState == MENU_STATE) {
+			drawMenuState(g);
+
+		} else if (currentState == GAME_STATE) {
+			drawGameState(g);
+		} else if (currentState == END_STATE) {
+			drawEndState(g);
+		}
 	}
-	
-	//Switch States
+
+	// Switch States
 	public void actionPerformed(ActionEvent e) {
-
 		repaint();
-//		if(currentState == MENU_STATE){
-//			updateMenuState();
-//		}else if (currentState == GAME_STATE){
-//			updateGameState();
-//		}else if(currentState == END_STATE){
-//			updateEndState();
-//		}
-//		
-		
+		if (currentState == MENU_STATE) {
+			updateMenuState();
+		} else if (currentState == GAME_STATE) {
+			updateGameState();
+		} else if (currentState == END_STATE) {
+			updateEndState();
+		}
+
 	}
 
-	
-	
-	//Key Controls
+	// Key Controls
 	@Override
 	public void keyTyped(KeyEvent e) {
 		System.out.println("key typed");
@@ -105,18 +102,42 @@ public class JumpPanel extends JPanel implements ActionListener, KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			objects.up = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			objects.down = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			objects.right = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			objects.left = true;
+		}
 		System.out.println("key pressed");
-//		if(e.getKeyCode() == KeyEvent.VK_ENTER){
-//			currentState++;
-//			if(currentState > END_STATE){
-//				currentState = MENU_STATE;
-//			}
-//			}
+		// if(e.getKeyCode() == KeyEvent.VK_ENTER){
+		// currentState++;
+		// if(currentState > END_STATE){
+		// currentState = MENU_STATE;
+		// }
+		// }
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		System.out.println("key released");
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			objects.up = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			objects.down = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			objects.right = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			objects.left = false;
+		}
+
 	}
 
 }
